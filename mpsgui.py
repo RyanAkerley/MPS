f4b7e2b2273e5ceda2a0d74ebc762a40e2a29993
Ryan Akerley <ryanakerley@gmail.com>
2020-02-07 03:18:15 -0500

    commit first file

---
+++ mpsgui.py
@@ -0,0 +1,24 @@
+# mpsgui.py
+
+# Ryan Akerley
+# Umass Boston Senior Design
+# ryan.akerley002@umb.edu
+
+import tkinter as tk
+
+main_window = tk.Tk()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main_window.mainloop()
