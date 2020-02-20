36e3ba8cfde3480ff991495f33ceaf343b910bee
Ryan Akerley <ryanakerley@gmail.com>
2020-02-20 15:33:45 -0500

    2/20/20 reorganizing folders

---
+++ src/mpsfunc.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+def export_csv():
+    print('The data has been exported as a CSV file')
+
+def set_freq(drive_freq):
+    print(f'The drive frequency has been set to {drive_freq}')
+
+def set_power(drive_power):
+    print(f'The drive power has been set to {drive_power}')
+
+
